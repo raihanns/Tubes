897f8699b8fa5e0746589590c2d7772dcf8a12e3
YusrilAG <61075909+YusrilAG@users.noreply.github.com>
2020-04-24 13:50:08 +0700

    update about us
    update about us

--- application/views/about.php
+++ application/views/about.php
@@ -11,14 +11,14 @@
 		border-radius: 0.5cm;
 	}
 
-	.tentangkami h1 {
+	.header h1 {
 		align-items: left;
 		font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
 		text-align: left;
 		margin-left: 25px;
 	}
 
-	.tentangkami p {
+	.header p {
 
 		font-size: large;
 		width: 90%;
@@ -37,39 +37,157 @@
 		font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
 		text-align: left;
 		margin-left: 25px;
+		padding-top: 20px;
+	}
+
+	.orang img {
+		padding-top: 10px;
+		padding-left: 10px;
+	}
+
+	.raihan {
+		border: 1px black double;
+	}
+
+
+	.raihan p {
+		font-size: large;
+		margin-left: 250px;
+		margin-top: -190px;
+		margin-bottom: 115px;
+		padding-left: 20px;
+
+	}
+
+	.yusril {
+		border: 1px black double;
+	}
+
+
+	.yusril p {
+		font-size: large;
+		margin-left: 250px;
+		margin-top: -180px;
+		margin-bottom: 115px;
+		padding-left: 20px;
+	}
+
+	.samsul {
+		border: 1px black double;
+	}
+
+
+	.samsul p {
+		font-size: large;
+		margin-left: 250px;
+		margin-top: -180px;
+		margin-bottom: 115px;
+		padding-left: 20px;
+	}
+
+	.adam {
+		border: 1px black double;
+	}
+
+
+	.adam p {
+		font-size: large;
+		margin-left: 250px;
+		margin-top: -180px;
+		margin-bottom: 115px;
+		padding-left: 20px;
+	}
+
+	.alek {
+		border: 1px black double;
+	}
+
+
+	.alek p {
+		font-size: large;
+		margin-left: 250px;
+		margin-top: -160px;
+		margin-bottom: 115px;
+		padding-left: 20px;
 	}
 </style>
 
 
 <div class="tentangkami">
-	<h1>HealtHub</h1>
-	<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis minus in ipsam corporis suscipit dignissimos
-		culpa qui
-		sit repudiandae doloremque facere, quibusdam officiis aperiam tenetur? Temporibus, doloremque provident.
-		Facilis, ea. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat debitis quas voluptatem
-		expedita voluptas.
-		Illum debitis nesciunt fugit ipsam laboriosam sapiente qui fuga.
-		Hic repellendus numquam vero quae provident mollitia. Lorem ipsum dolor sit amet consectetur adipisicing
-		elit.
-		Beatae expedita possimus voluptatibus, eum molestiae obcaecati illum in magnam architecto error laboriosam
-		illo numquam, p
-		ariatur facere eius, adipisci blanditiis consequatur <suscipit class="lorem"></suscipit>
-	</p>
-
+	<div class="header">
+		<h1>HealtHub</h1>
+		<p>
+			Web ini bertujuan untuk menunjang Tugas Besar Pemrograman Web. Kami melihat masalah yang terjadi pada pasien
+			yang ingin rujuk rumah sakit tetapi terkendala di masalah rumah sakit mana yang masih ada kamar ,
+			maka dari itu kami membuat HealthHub ini agar masyarakat yang ingin rujuk rumah sakit bisa
+			dengan mudah mencari informasi tersebut pada website kami.
+		</p>
+	</div>
 	<div class="orang">
 		<h2>Siapa Pendiri HealtHub?</h2>
-		<p>
-			Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis minus in ipsam corporis suscipit
-			dignissimos culpa qui
-			sit repudiandae doloremque facere, quibusdam officiis aperiam tenetur? Temporibus, doloremque provident.
-			Facilis, ea. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat debitis quas voluptatem
-			expedita voluptas.
-			Illum debitis nesciunt fugit ipsam laboriosam sapiente qui fuga.
-			Hic repellendus numquam vero quae provident mollitia. Lorem ipsum dolor sit amet consectetur adipisicing
-			elit.
-			Beatae expedita possimus voluptatibus, eum molestiae obcaecati illum in magnam architecto error
-			laboriosam illo numquam, p
-			ariatur facere eius, adipisci blanditiis consequatur
-		</p>
+		<div class="foto">
+			<div class="raihan">
+				<img src="<?= base_url() . 'gambar/raihan.jpg' ?>" class="rounded-circle" width="200px">
+				<p><b> Nama : Raihan Nugraha , Kelas : IF-42-01 , Nim : 1301184243 .</b>
+					<br>
+					<br>
+					Saya berasal dari Cirebon, dan saya kuliah di Universitas Telkom Bandung. Sekarang ini saya
+					sedang berada di semester 4. Disini saya dan teman-teman saya mengerjakan Web ini untuk memenuhi
+					Tugas Besar Pemrograman Web
+				</p>
+			</div>
+			<br>
+			<br>
+			<br>
+			<div class="yusril">
+				<img src="<?= base_url() . 'gambar/yusril.jpg' ?>" class="rounded-circle" width="200px">
+				<p><b> Nama : Yusril Alauddin Ghani , Kelas : IF-42-01 , Nim : 1301184217 .</b>
+					<br>
+					<br>
+					Saya berasal dari Bekasi, dan saya kuliah di Universitas Telkom Bandung. Sekarang ini saya
+					sedang berada di semester 4. Disini saya dan teman-teman saya mengerjakan Web ini untuk memenuhi
+					Tugas Besar Pemrograman Web
+				</p>
+			</div>
+			<br>
+			<br>
+			<br>
+			<div class="samsul">
+				<img src="<?= base_url() . 'gambar/samsul.jpg' ?>" class="rounded-circle" width="200px">
+				<p><b> Nama : Syamsul Rizal , Kelas : IF-42-01 , Nim : 1301184064 .</b>
+					<br>
+					<br>
+					Saya berasal dari Bandung, dan saya kuliah di Universitas Telkom Bandung. Sekarang ini saya
+					sedang berada di semester 4. Disini saya dan teman-teman saya mengerjakan Web ini untuk memenuhi
+					Tugas Besar Pemrograman Web
+				</p>
+			</div>
+			<br>
+			<br>
+			<br>
+			<div class="adam">
+				<img src="<?= base_url() . 'gambar/adam.jpg' ?>" class="rounded-circle" width="200px">
+				<p><b> Nama : Adham Erlangga Putra , Kelas : IF-42-01 , Nim : 1301184198 .</b>
+					<br>
+					<br>
+					Saya berasal dari Bekasi, dan saya kuliah di Universitas Telkom Bandung. Sekarang ini saya
+					sedang berada di semester 4. Disini saya dan teman-teman saya mengerjakan Web ini untuk memenuhi
+					Tugas Besar Pemrograman Web
+				</p>
+			</div>
+			<br>
+			<br>
+			<br>
+			<div class="alek">
+				<img src="<?= base_url() . 'gambar/alek.jpg' ?>" class="rounded-circle" width="200px">
+				<p><b> Nama : Ridhwan Nashir, Kelas : IF-42-01 , Nim : 1301184259 .</b>
+					<br>
+					<br>
+					Saya berasal dari Cimahi, dan saya kuliah di Universitas Telkom Bandung. Sekarang ini saya
+					sedang berada di semester 4. Disini saya dan teman-teman saya mengerjakan Web ini untuk memenuhi
+					Tugas Besar Pemrograman Web
+				</p>
+			</div>
+		</div>
 	</div>
 </div>
