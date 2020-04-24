c04e2276619c7391c3e4a28158eece74a704de43
Dame <62693001+adhamerlangga@users.noreply.github.com>
2020-04-24 17:52:56 +0700

    Nambahin Foto Yusril Sama titlenya jadi about us

--- application/controllers/About.php
+++ application/controllers/About.php
@@ -6,8 +6,8 @@ class About extends CI_Controller
 
     public function index()
     {
-        $data['title'] = 'About';
-        $this->load->view('templates/auth_header');
+        $data['title'] = 'About Us';
+        $this->load->view('templates/auth_header',$data);
         $this->load->view('about');
         $this->load->view('templates/auth_footer');
     }
